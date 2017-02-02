07b9862399da94f1d97e5ec920e6986eeb42e48a
SangeethaRam <1316116@saec.ac.in>
2017-02-01 23:00:52 -0800

    Add files via upload

---
+++ ideone_UHiYQX.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	printf("\n positive");
+	if(n<0)
+	printf("\n negative");
+	if(n==0)
+	printf("\n zero");
+	return 0;
+}
