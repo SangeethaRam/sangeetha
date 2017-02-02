ea2b5d1e9e881e8aee138fc11653f71d91e40687
SangeethaRam <1316116@saec.ac.in>
2017-02-01 23:21:16 -0800

    Add files via upload

---
+++ ideone_zoKsAF.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char c;
+	scanf("%c",&c);
+	if((c=='a')|(c=='e')|(c=='i')|(c=='o')|(c=='u'))
+	printf("\nvowel");
+	else
+	printf("\n constant");
+	return 0;
+}
